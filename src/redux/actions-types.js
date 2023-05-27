6b9ba8e1f939755beaf3773510d04f48b9bad58c
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-05-26 20:35:49 -0400

    Delete src directory

--- src/redux/actions-types.js
+++
@@ -1,5 +0,0 @@
-export const AGREGAR_CITA = "AGREGAR_CITA";
-export const EDITAR_CITA = "EDITAR_CITA";
-export const BUSCAR_CITA = "BUSCAR_CITA";
-export const LISTAR_CITAS = "LISTAR_CITAS";
-export const ELIMINAR_CITA = "ELIMINAR_CITA";
