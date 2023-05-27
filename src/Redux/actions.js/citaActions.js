350fea46bcecb05ca4127ef8b0c80e89766c5229
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-05-26 20:50:53 -0400

    Delete src directory

--- src/Redux/actions.js/citaActions.js
+++
@@ -1,8 +0,0 @@
-export const deleteCita = (id) => {
-    return {
-      type: "ELIMINAR_CITA",
-      payload: id,
-    };
-  };
-  
-  // acción de eliminar en el archivo 
--- src/Variables.js
+++
@@ -1,6 +0,0 @@
-export const colorSection= "#fff";
-export const colorText= "#444655";
-export const colorPrimary= "#DD4962";
-export const colorSecundary= "#4a7ae5";
-export const coloBackground= "#a8aabc";
-export const fontFamily= "Goudy Bookletter 1911, serif";
