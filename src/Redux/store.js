be7accf35b4ec4ce099bbfd3031bff292ef877d9
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-14 22:38:34 -0400

    Delete src/Redux directory

--- src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducer/reducers";
-
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
-const store = createStore(rootReducer, composedEnhancer);
-
-export default store;
