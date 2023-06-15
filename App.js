c724e3a734f5ee5c7eccd9e49443b4396f88d2be
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-15 14:30:05 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
 import Detail from './components/Detail/detail';
 import About from './pages/About/about';
@@ -9,6 +11,24 @@ import List from './components/List/list';
 import Login from './components/Login';
 
 function App() {
+
+  const navigate = useNavigate();
+   const [access, setAccess] = useState(true);
+
+   function login(userData) {
+      const { username, password } = userData;
+      const URL = 'http://localhost:3001/api/';
+      axios.post(URL, { username, password }).then(({ data }) => {
+         const { access } = data;
+         setAccess(access);
+         access && navigate('/menu');
+      });
+   }
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
 
   return (
     <div className="app">
