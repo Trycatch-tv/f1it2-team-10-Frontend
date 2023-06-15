0006cbc4eabcd766e07b35bea43011b6faa77fdf
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-14 22:41:25 -0400

    Delete src/api directory

--- src/api/api.js
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios';
-
-// const api = axios.create({
-//   baseURL: 'https://tu-backend-url.com/api',
-// });
-
-// export default api;
--- src/api/citas.js
+++
@@ -1,6 +0,0 @@
-// import api from './api';
-
-// export const getCitas = () => api.get('/citas');
-// export const createCita = (cita) => api.post('/citas', cita);
-// export const updateCita = (id, cita) => api.put(`/citas/${id}`, cita);
-// export const deleteCita = (id) => api.delete(`/citas/${id}`);
