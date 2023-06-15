2b72e58930116849f35d7e9ba0ddfefe228ace04
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-15 15:11:12 -0400

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import reducer from './reducer';
+import reducer from './reducer/reducers';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // esta línea sirve para conectar nuestra App con la extensión REDUX DEVTOOLS DEL NAVEGADOR
 
@@ -9,10 +9,6 @@ const store = createStore(
     reducer,
     composeEnhancer(applyMiddleware(thunkMiddleware)) // esta línea sirva para que podamos hacer peticiones a una Api/servidor
 );
-
-
-
-export default store;
 
 
 export default store;
