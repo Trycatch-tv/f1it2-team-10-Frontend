0c6eeb60f8406c3209469eb58d1573c09da0f2ed
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-16 22:24:10 -0400

    Update actions.js

--- redux/actions/actions.js
+++ redux/actions/actions.js
@@ -1,28 +1,27 @@
 import axios from 'axios';
-import { GET_APPOINTMENT, CREATE_APPOINTMENT, EDIT_APPOINTMENT, DELETE_APPOINTMENT } from "./action-types";
+import { CREAR_CITA, ACTUALIZAR_CITA, ELIMINAR_CITA, LISTAR_CITAS } from "./action-types";
 
-export const createAppointment = (appointment) => {
-  return { type: CREATE_APPOINTMENT, payload: appointment };
+export const crearCita = (cita) => {
+  return { type: CREAR_CITA, payload: cita };
 };
   
-export const editAppointment = (id, appointment) => {
-  return { type: EDIT_APPOINTMENT, payload: { id, appointment } };
+export const actualizarCita = (id, cita) => {
+  return { type: ACTUALIZAR_CITA, payload: { id, cita } };
 };
   
-export const deleteAppointment = (id) => {
-  return { type: DELETE_APPOINTMENT, payload: id };
+export const eliminarCita = (id) => {
+  return { type: ELIMINAR_CITA, payload: id };
 };
 
-const getAppointment = () => {
+const getCita = () => {
   return async function (dispatch) {
     try {
       let response = await axios.get("https://your-api-endpoint");
-      return dispatch({ type: GET_APPOINTMENT, payload: response.data.results });
+      return dispatch({ type: LISTAR_CITAS, payload: response.data.results });
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 };
 
-export default getAppointment;
-  
+export default getCita;
