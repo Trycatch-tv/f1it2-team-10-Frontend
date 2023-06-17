ac013637751abb28b28c5f7bd2166e09c0709420
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-16 22:25:54 -0400

    Delete api directory

--- api/utils/citas.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const getAppointments = () => api.get('/appointment');
-export const createAppointment = (appointment) => api.post('/appointments', appointment);
-export const editAppointment = (id, appointment) => api.put(`/appointments/${id}`, appointment);
-export const deleteAppointment = (id) => api.delete(`/appointments/${id}`);
