24bb799c32d32da75862d5aaa4a652648729e3b1
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-06-24 20:27:50 -0400

    Delete redux directory

--- redux/actions/action-types.js
+++
@@ -1,5 +0,0 @@
-export const CREAR_CITA = 'CREAR_CITA';
-export const ACTUALIZAR_CITA = 'ACTUALIZAR_CITA';
-export const ELIMINAR_CITA = 'ELIMINAR_CITA';
-export const LISTAR_CITAS = 'LISTAR_CITAS';
-export const GET_CITA = 'GET_CITA';
