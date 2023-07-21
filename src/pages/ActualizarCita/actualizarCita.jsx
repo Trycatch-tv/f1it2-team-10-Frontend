3d7d089ed5f1543df967061f0b8202d37e492d5f
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-07-20 22:47:27 -0400

    Update actualizarCita.jsx

--- src/pages/ActualizarCita/actualizarCita.jsx
+++ src/pages/ActualizarCita/actualizarCita.jsx
@@ -1,60 +1,11 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { crearCita, actualizarCita, recuperarCita, eliminarCita } from '../../redux/actions/actions';
-import CrearCita from '../CrearCita/crearCita';
+import { actualizarCita } from '../../redux/actions/actions';
 import axios from 'axios';
 import './actualizarCita.css';
 
-export default function Actualizar() {
-  const citas = useSelector((state) => state.citas);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleActualizarCita = useCallback((cita) => {
-    const citaToUpdate = citas.find((c) => c.id === cita.id);
-    if (citaToUpdate) {
-      dispatch(actualizarCita(cita));
-      navigate('/detalle');
-    } else {
-      console.error('Cita to update not found');
-    }
-  }, [citas, dispatch, navigate]);
-  
-  const handleGuardarCita = useCallback((cita) => {
-    const citaExists = citas.some((c) => c.id === cita.id);
-    if (citaExists) {
-      console.error('Cita with the same id already exists');
-    } else {
-      dispatch(crearCita(cita));
-      navigate('/detalle');
-    }
-  }, [citas, dispatch, navigate]);
-
-  const handleRecuperarCita = useCallback((id) => {
-    const cita = citas.find((cita) => cita.id === id);
-    if (cita) {
-      dispatch(recuperarCita(cita));
-      navigate('/detalle');
-    } else {
-      console.error('Cita not found');
-    }
-  }, [citas, dispatch, navigate]);
-
-  const handleEliminarCita = useCallback((id) => {
-    const cita = citas.find((cita) => cita.id === id);
-    if (cita) {
-      dispatch(eliminarCita(cita));
-      navigate('/exit');
-    } else {
-      console.error('Cita not found');
-    }
-  }, [citas, dispatch, navigate]);
-
-  const [citaSelectId, setCitaSelectId] = useState(null);
-  const citaSelect = useSelector((state) => 
-    state.citas.find((cita) => cita.id === citaSelectId));
-
+export default function ActualizarCita() {
   const [cita, setCita] = useState({
     id: '',
     nombre: '',
@@ -63,134 +14,163 @@ export default function Actualizar() {
     duracion: '',
     ubicacion: '',
     detalles: '',
-    estado: ''
+    estado: '',
   });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCita((prevCita) => ({
-      ...prevCita,
-      [name]: value
-    }));
-  };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('/citas', cita)
+    axios
+      .put(`/citas/${cita.id}`, cita) 
       .then((response) => {
         console.log(response.data);
         navigate('/detalle');
-      })
-      .catch((error) => {
-        console.error(error);
       });
   };
 
+  const handleInputChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setCita((prevCita) => ({
+      ...prevCita,
+      [name]: value,
+    }));
+  }, []);
+
+  const handleActualizarCita = useCallback(() => {
+    dispatch(actualizarCita(cita));
+  }, [cita, dispatch]);
+
+  const handleSaveClick = useCallback(() => {
+    dispatch(actualizarCita()); 
+    navigate('/detalle');
+  }, [dispatch, navigate]);
+
+  const handleMenuClick = useCallback(() => {
+    navigate('/menu');
+  }, [navigate]);
+
   return (
-    <div className="cita-form">
-      <form onSubmit={handleSubmit}>
-        <h2 className="encabezado">Actualizar cita</h2>
-        <div className="input-container">
-          <label htmlFor="id" className="label">
-            Id:
+    <div className="background-forms">
+      <div className="citas-forms">
+        <form onSubmit={handleSubmit}>
+          <div className="encabezados">
+            <h2>Actualizar cita</h2>
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="id">Id:</label>
             <input
-              type="number"
+              type="text"
+              id="id"
               name="id"
               value={cita.id}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="nombre" className="label">
-            Nombre:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="nombre">Nombre:</label>
             <input
               type="text"
+              id="nombre"
               name="nombre"
               value={cita.nombre}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="fecha" className="label">
-            Fecha:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="fecha">Fecha:</label>
             <input
               type="date"
+              id="fecha"
               name="fecha"
               value={cita.fecha}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="hora" className="label">
-            Hora:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="hora">Hora:</label>
             <input
               type="time"
+              id="hora"
               name="hora"
               value={cita.hora}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="ubicacion" className="label">
-            Ubicación:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="duracion">Duración:</label>
             <input
-              type="text"
-              name="ubicacion"
-              value={cita.ubicacion}
-              onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="duracion" className="label">
-          Duración:
-            <input
-              type="text"
+              type="number"
+              id="duracion"
               name="duracion"
               value={cita.duracion}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="detalles" className="label">
-            Detalles:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="ubicacion">Ubicación:</label>
+            <input
+              type="text"
+              id="ubicacion"
+              name="ubicacion"
+              value={cita.ubicacion}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="detalles">Detalles:</label>
             <textarea
+              id="detalles"
               name="detalles"
               value={cita.detalles}
               onChange={handleInputChange}
-              required/>
-          </label>
-        </div>
-        <div className="input-container">
-          <label htmlFor="estado" className="label">
-            Estado:
+              required
+            />
+          </div>
+          <div className="inputs-containers">
+            <label htmlFor="estado">Estado:</label>
             <select
               name="estado"
+              id="estado"
               value={cita.estado}
               onChange={handleInputChange}
-              required>
+              required
+            >
               <option value="">Seleccione un estado</option>
               <option value="Pendiente">Pendiente</option>
               <option value="Confirmada">Confirmada</option>
               <option value="Cancelada">Cancelada</option>
             </select>
-          </label>
-        </div>
+          </div>
+          <div className="buttons-container">
+            <button type="button" className="buttons-clicked" onClick={handleActualizarCita}>
+              Actualizar Cita
+            </button>
+          </div>
+          <div className="buttons-container">
+            <button type="button" className="buttons-clicked" onClick={handleSaveClick}>
+              Guardar Cita
+            </button>
+          </div>
         <div className="button-container">
-          <button type="submit" className="button-submit">Guardar</button>
+          <button type="button" className="buttons-clicked" onClick={handleMenuClick}>
+            Menu
+          </button>
         </div>
-      </form>
-      <CrearCita
-        cita={citaSelect}
-        handleActualizarCita={handleActualizarCita}
-        handleGuardarCita={handleGuardarCita}
-        handleRecuperarCita={handleRecuperarCita}
-        handleEliminarCita={handleEliminarCita}/>
+        <div className="buttons-container">
+          <button type="button" className="buttons-clicked" onClick={() => navigate('/')}>
+            Exit
+          </button>
+        </div>
+        </form>
+      </div>
     </div>
   );
 }
