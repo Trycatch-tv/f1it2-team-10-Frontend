c0aeba969eb106f8f3188d01a437296ffa4fbd7d
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-07-20 22:44:20 -0400

    Update actions.js

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,36 +1,68 @@
 import axios from 'axios';
-import { CREAR_CITA, ACTUALIZAR_CITA, ELIMINAR_CITA, BUSCAR_CITAS, RECUPERAR_CITA } from "./action-types";
 
-export const crearCita = (cita) => {
-  return { type: CREAR_CITA, payload: cita };
-};
-  
-export const actualizarCita = (id, cita) => {
-  return { type: ACTUALIZAR_CITA, payload: { id, cita }};
-};
-  
-export const eliminarCita = (id) => {
-  return { type: ELIMINAR_CITA, payload: id };
-};
+export const CREAR_CITA = 'CREAR_CITA';
+export const GET_CITAS = 'GET_CITAS';
+export const ACTUALIZAR_CITA = 'ACTUALIZAR_CITA';
+export const ELIMINAR_CITA = 'ELIMINAR_CITA';
+export const GET_CITA = 'GET_CITA';
 
-export const buscarCitas = (terminoBusqueda) => {
+export const crearCita = (payload) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get(`/citas?q=${terminoBusqueda}`);
-      dispatch({ type: BUSCAR_CITAS, payload: response.data });
+      const response = await axios.post('/api/citas', payload);
+      dispatch({ type: CREAR_CITA, payload: response.data });
     } catch (error) {
-      console.log(error);
+      console.error('Error al crear cita:', error);
+      throw error; 
+    }
+  }
+}
+
+export const getCitas = () => {
+  return async function (dispatch) {
+    try {
+        const response = await axios.get('/api/citas');
+        dispatch({
+            type: GET_CITAS,
+            payload: response.data
+        })
+    } catch (error) {
+        console.error('Error al obtener citas:', error);
+        throw error; 
+    }
+  }
+};
+
+export const actualizarCita = (id, cita) => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.put(`/api/citas/${id}`, cita);
+      dispatch({ type: ACTUALIZAR_CITA, payload: response.data });
+    } catch (error) {
+        console.error(`Error al actualizar cita con id=${id}:`, error);
+        throw error; 
     }
   };
 };
 
-export const recuperarCita = () => {
+export const eliminarCita = (id) => {
   return async function (dispatch) {
     try {
-      const response = await axios.get("/citas");
-      dispatch({ type: RECUPERAR_CITA, payload: response.data });
+      await axios.delete(`/api/citas/${id}`);
+      dispatch({ type: ELIMINAR_CITA, payload: id });
     } catch (error) {
-      console.log(error);
+      console.error(`Error al eliminar cita con id ${id}:`, error);
+    }
+  };
+};
+
+export const getCita = (id) => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`/api/citas/${id}`);
+      dispatch({ type: GET_CITA, payload: response.data });
+    } catch (error) {
+      console.error(`Error al obtener cita con id ${id}:`, error);
     }
   };
 };
