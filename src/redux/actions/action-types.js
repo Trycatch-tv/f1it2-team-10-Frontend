4c6c31a4c45ced65764998a98b8e4d6e84253932
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-07-20 22:43:14 -0400

    Delete action-types.js

--- src/redux/actions/action-types.js
+++
@@ -1,5 +0,0 @@
-export const CREAR_CITA = 'CREAR_CITA';
-export const ACTUALIZAR_CITA = 'ACTUALIZAR_CITA';
-export const ELIMINAR_CITA = 'ELIMINAR_CITA';
-export const BUSCAR_CITAS = 'BUSCAR_CITAS';
-export const RECUPERAR_CITA = 'RECUPERAR_CITA';
